1a70cb34e4614c7a41b252b8fa085189bf047c2f
Jake Niemiec <jake.j.niemiec@gmail.com>
2016-06-15 11:14:41 -0500

    feat: init commit

---
+++ jspm.config.js
@@ -0,0 +1,46 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "github:": "jspm_packages/github/",
+    "app/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "app": {
+      "main": "app.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {
+    "bootstrap": "github:twbs/bootstrap@4.0.0-alpha.2",
+    "jquery": "npm:jquery@2.2.4"
+  },
+  packages: {
+    "github:twbs/bootstrap@4.0.0-alpha.2": {
+      "map": {
+        "jquery": "npm:jquery@2.2.4",
+        "tether": "github:HubSpot/tether@1.3.2"
+      }
+    }
+  }
+});
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import bootstrap from "bootstrap";
+
+console.log("Bootstrap:",bootstrap);
+console.log("jQuery aliased as `$`",$);
+console.log("Test jQuery",$('body'));
